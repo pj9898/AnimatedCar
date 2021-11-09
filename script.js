b40f189cc51c7339e632c0eabe14439993393f65
pj9898 <jprabhat65@gmail.com>
2021-11-09 15:23:42 +0530

    Updated code

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function sound(){
+/*var aud=document.createElement('audio');
+aud.setAttribute('src','sound.mp4');
+aud.loop=true;
+aud.play();
+*/
+var audio = new Audio('sound.mp4');
+  audio.play();
+}
